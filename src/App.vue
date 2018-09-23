255aaf2d3181a35a873be6ed014281dbe7f13416
Neil Agarwal <regalstreak@gmail.com>
2018-09-23 19:45:49 +0530

    aspire2018: Bring up parallax

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,14 @@
       app
       :clipped-left="clipped"
     >
-      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-toolbar-title><font style="font-family: 'Redressed; font-size: 2rem;'">{{title}}</font></v-toolbar-title>
     </v-toolbar>
     <v-content>
       <router-view/>
     </v-content>
-    <v-footer :fixed="fixed" app>
+    <v-footer >
       <v-layout column align-center>
-        <pre><b>&lt;/&gt;</b> with ❤️ by <b>Neil Agarwal</b></pre>
+        <pre><b>&lt;/&gt;</b> with <v-icon class="red--text" size="17">favorite</v-icon>️ by <b>Neil Agarwal</b></pre>
       </v-layout>
     </v-footer>
   </v-app>
@@ -27,3 +27,17 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+mainhdr {
+  color: black;
+  text-align: center;
+  font-weight: bolder;
+  font-size: 1rem;
+  letter-spacing: 1px;
+  overflow: auto;
+  margin-top: 0px;
+  padding: 0px;
+  font-family: "M PLUS Rounded 1c", sans-serif;
+}
+</style>
