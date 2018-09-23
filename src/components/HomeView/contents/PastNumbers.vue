2806805f29cec63c42be0cccbfb3d3f5a7b64d9f
Neil Agarwal <regalstreak@gmail.com>
2018-09-24 01:02:11 +0530

    src: Before i get mad lemme refactor more

--- src/components/HomeView/contents/PastNumbers.vue
+++ src/components/HomeView/contents/PastNumbers.vue
@@ -1,39 +1,128 @@
 <template>
   <section>
     <v-parallax :src="`${baseUrl}parallax/section.jpg`" height="380">
-      <v-layout column align-center justify-center>
-        <div class="headline white--text mb-3 text-xs-center">Past Numbers</div>
 
-        <v-layout row wrap>
-          <v-flex v-for="card in cards" v-bind="{ [`xs${card.flex}`]: true }" :key="card.title">
-            <v-card>
-              <v-img :src="card.src" height="200px">
-                <v-container fill-height fluid pa-2>
-                  <v-layout fill-height>
-                    <v-flex xs12 align-end flexbox>
-                      <span class="headline white--text" v-text="card.title"></span>
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-img>
+      <v-container wrap>
 
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn icon>
-                  <v-icon>favorite</v-icon>
-                </v-btn>
-                <v-btn icon>
-                  <v-icon>bookmark</v-icon>
-                </v-btn>
-                <v-btn icon>
-                  <v-icon>share</v-icon>
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
-        </v-layout>
+        <v-flex class="text-xs-center">
+          <div class="display-1 white--text mb-3 text-xs-center">
+            Past Numbers
+          </div>
+          <br>
 
-      </v-layout>
+      <v-flex xs12>
+        <v-container grid-list-xl>
+          <v-layout row align-center>
+          
+            <v-flex xs6 lg3 mx-5>
+
+              <v-card>
+                <v-flex py-2>
+                <v-icon x-large color="green darken-2">
+                  business
+                </v-icon>
+                </v-flex>
+                <v-flex pa-4>
+                
+                  <div class="display-3 text-xs-center">
+                    {{4 * 100 / 5}}
+                  </div>
+                
+                </v-flex>
+                <v-flex py-1>
+                
+                  <div class="headline text-xs-center">
+                    Events
+                  </div>
+                
+                </v-flex>
+                
+              </v-card>
+            </v-flex>
+            <v-flex xs6 lg3 mx-5>
+
+              <v-card>
+                <v-flex py-2>
+                <v-icon x-large color="green darken-2">
+                  business
+                </v-icon>
+                </v-flex>
+                <v-flex pa-4>
+                
+                  <div class="display-3 text-xs-center">
+                    {{4 * 100 / 5}}
+                  </div>
+                
+                </v-flex>
+                <v-flex py-1>
+                
+                  <div class="headline text-xs-center">
+                    Events
+                  </div>
+                
+                </v-flex>
+                
+              </v-card>
+            </v-flex>
+            <v-flex xs12 lg3 mx-5>
+
+              <v-card>
+                <v-flex py-2>
+                <v-icon x-large color="green darken-2">
+                  business
+                </v-icon>
+                </v-flex>
+                <v-flex pa-4>
+                
+                  <div class="display-3 text-xs-center">
+                    {{4 * 100 / 5}}
+                  </div>
+                
+                </v-flex>
+                <v-flex py-1>
+                
+                  <div class="headline text-xs-center">
+                    Events
+                  </div>
+                
+                </v-flex>
+                
+              </v-card>
+            </v-flex>
+            <v-flex xs12 lg3 mx-5>
+
+              <v-card>
+                <v-flex py-2>
+                <v-icon x-large color="green darken-2">
+                  business
+                </v-icon>
+                </v-flex>
+                <v-flex pa-4>
+                
+                  <div class="display-3 text-xs-center">
+                    {{4 * 100 / 5}}
+                  </div>
+                
+                </v-flex>
+                <v-flex py-1>
+                
+                  <div class="headline text-xs-center">
+                    Events
+                  </div>
+                
+                </v-flex>
+                
+              </v-card>
+            </v-flex>
+
+          </v-layout>
+        </v-container>
+      </v-flex>
+
+
+        </v-flex>
+
+      </v-container>
     </v-parallax>
   </section>
 </template>
@@ -41,23 +130,11 @@
 <script>
 export default {
   data: () => ({
-    cards: [
-      {
-        title: "Pre-fab homes",
-        src: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
-        flex: 12
-      },
-      {
-        title: "Favorite road trips",
-        src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
-        flex: 6
-      },
-      {
-        title: "Best airlines",
-        src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
-        flex: 6
-      }
-    ]
+    cards: {
+      title: "Pre-fab homes",
+      src: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
+      flex: 12
+    }
   }),
   computed: {
     baseUrl: () => {
@@ -66,3 +143,12 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.element {
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+  /* or try 50% */
+}
+</style>
